d854766893c7b08ca0eecfe79910ef38cc99b766
dsfank <dsfank@live.com>
2014-07-21 16:31:24 -0500

    added a test class

---
+++ Git Test/DFTest.h
@@ -0,0 +1,13 @@
+//
+//  DFTest.h
+//  Git Test
+//
+//  Created by Daren Fankhauser on 7/21/14.
+//  Copyright (c) 2014 Daren Fankhauser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DFTest : NSObject
+
+@end
---
+++ Git Test/DFTest.m
@@ -0,0 +1,13 @@
+//
+//  DFTest.m
+//  Git Test
+//
+//  Created by Daren Fankhauser on 7/21/14.
+//  Copyright (c) 2014 Daren Fankhauser. All rights reserved.
+//
+
+#import "DFTest.h"
+
+@implementation DFTest
+
+@end
