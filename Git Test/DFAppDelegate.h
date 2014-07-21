206b963bd60f474fac2a3389928a325d931d8f15
Daren Fankhauser <dfankhauser@Daren-Fankhausers-MacBook-Pro.local>
2014-07-21 12:32:13 -0500

    Initial Commit

---
+++ Git Test/DFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DFAppDelegate.h
+//  Git Test
+//
+//  Created by Daren Fankhauser on 7/21/14.
+//  Copyright (c) 2014 Daren Fankhauser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
